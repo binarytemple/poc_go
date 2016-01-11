a81af07abdc66f8d9e695d470090946643cce2a6
bryan hunt <github@binarytemple.co.uk>
2016-01-11 12:12:33 +0000

    read and concatenate file contents

--- list_files_example/read_leases.go
+++ list_files_example/read_leases.go
@@ -4,12 +4,17 @@ import "fmt"
 import "io/ioutil"
 
 func main() {
-  
-  files, _ := ioutil.ReadDir("./tests")
+  path := "./tests" 
+  files, _ := ioutil.ReadDir(path)
+  var all = ""
   for _, f := range files {
     var fileName = f.Name() 
     if( fileName[len(fileName)-7:] == ".leases" ) {
+        content,_ := ioutil.ReadFile( path + "/" + fileName)   
         fmt.Println(fileName)
+        fmt.Println(string(content))
+        all = all + string(content)
     }
   }
+  fmt.Println(all)
 }
